a82209d3b0a06ff422bacdca9610d984b6507963
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-30 19:57:38 -0500

    requires the full path... wish I knew why

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 
-service = Service(executable_path = "chromedriver")
+service = Service(executable_path = "/Users/jasoncameron/Desktop/Learning/Exploration/Exploring_Selenium/chromedriver")
 driver = webdriver.Chrome(service = service)
 
 driver.get("https://google.com")
