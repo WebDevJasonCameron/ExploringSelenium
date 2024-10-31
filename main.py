68c42141050f9eefef532659cb6b6d7b7a51f81d
Jason Cameron <dev.jasoncameron@gmail.com>
2024-10-31 17:54:27 -0500

    working more

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By 
 import time
 
 service = Service(executable_path = "/Users/jasoncameron/Desktop/Learning/Exploration/Exploring_Selenium/chromedriver")
 driver = webdriver.Chrome(service = service)
 
 driver.get("https://google.com")
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("tech with tim")
 
 time.sleep(10)
 
