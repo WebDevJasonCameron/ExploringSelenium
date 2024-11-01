8c5d82cac29faf176384e94551d709450bb3d515
Jason Cameron <dev.jasoncameron@gmail.com>
2024-11-01 18:25:14 -0500

    enter added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service = Service(executable_path = "/Users/jasoncameron/Desktop/Learning/Exploration/Exploring_Selenium/chromedriver")
@@ -9,7 +10,7 @@ driver = webdriver.Chrome(service = service)
 driver.get("https://google.com")
 
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("tech with tim")
+input_element.send_keys("tech with tim" + Keys.ENTER)
 
 time.sleep(10)
 
