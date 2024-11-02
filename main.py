d9ee71f0b6c776a5d6085f0cf0d2803333a25efa
Jason Cameron <dev.jasoncameron@gmail.com>
2024-11-02 12:43:42 -0500

    add a wait and check

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path = "/Users/jasoncameron/Desktop/Learning/Exploration/Exploring_Selenium/chromedriver")
@@ -9,8 +11,17 @@ driver = webdriver.Chrome(service = service)
 
 driver.get("https://google.com")
 
+WebDriverWait(driver, 5).until(
+  EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
 input_element.send_keys("tech with tim" + Keys.ENTER)
+
+WebDriverWait(driver, 5).until(
+  EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
 
 time.sleep(10)
 
